ecb242085251abd9b4c88bed552c4cf377f82231
acher <shuiqingliang@outlook.com>
2021-01-06 20:03:27 +0800

    java形式的全排列

---
+++ Week_03/permute_2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+        boolean[] used = new boolean[len];
+        List<Integer> path = new ArrayList<>();
+        dfs(nums, len, 0, path, used, res);
+        return res;
+    }
+    private void dfs(int[] nums, int len, int depth,  List<Integer> path, boolean[] used, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        // 在非叶子结点处，产生不同的分支
+        // 在还未选择的数中依次选择一个元素作为下一个位置的元素
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                path.add(nums[i]);
+                used[i] = true;
+                dfs(nums, len, depth + 1, path, used, res);
+                // 下面这两行代码发生 「回溯」，回溯发生在从 深层结点 回到 浅层结点 的过程
+                // 代码在形式上和递归之前是对称的
+                path.remove(path.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
+}
